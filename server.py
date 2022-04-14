634c4c7580111fa5ff01289480e2248e12f637ec
Michael <congcong0716@sina.com>
2022-04-14 12:40:52 -0400

    add routes

--- server.py
+++ server.py
@@ -11,9 +11,21 @@ def index():
     return render_template('index.html')
 
 
+# Learning routes
+
 @app.route('/learn')
 def learn():
     return render_template('learn.html')
+
+
+@app.route('/learn/technique1/level')
+def technique1_level():
+    pass
+
+
+@app.route('/learn/technique1')
+def technique1():
+    pass
 
 
 if __name__ == '__main__':
