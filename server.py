9d3496305ff8120f2c718b78cc1c86fdb2f58db0
Michael <congcong0716@sina.com>
2022-04-14 12:33:47 -0400

    add homepage & learning homepage;

--- server.py
+++ server.py
@@ -11,5 +11,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
