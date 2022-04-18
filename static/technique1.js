fd1d9d0cdd3f5d6fcc1a64f82e7392737844b4be
bigapple716 <congcong0716@outlook.com>
2022-04-18 13:18:05 -0400

    change loc of data/; add white noise mp3 file

--- static/technique1.js
+++ static/technique1.js
@@ -1,24 +1,3 @@
-function audio() {
-  let audio = new Audio("http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3");
-
-  $('#play-pause-button').on("click", function () {
-    if ($(this).hasClass('fa-play')) {
-      $(this).removeClass('fa-play');
-      $(this).addClass('fa-pause');
-      audio.play();
-    } else {
-      $(this).removeClass('fa-pause');
-      $(this).addClass('fa-play');
-      audio.pause();
-    }
-  });
-
-  audio.onended = function () {
-    $("#play-pause-button").removeClass('fa-pause');
-    $("#play-pause-button").addClass('fa-play');
-  };
-}
-
 function timer() {
   'use strict';
 
@@ -138,7 +117,6 @@ function check_first_last_paragraph(data) {
 }
 
 $(document).ready(function () {
-  audio();
   timer();
   check_first_last_paragraph(data);
 })
